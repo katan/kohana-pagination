43308dc72a3bfe137a65e6e6dadaca189b4690eb
Ricard Catalan <zicard@gmail.com>
2012-07-25 17:39:00 +0200

    Fixed problem with routing.  - See config file to how to do.

--- README.md
+++ README.md
@@ -3,5 +3,27 @@ Kohana 3.2 Pagination
 
 This is hacked up to support Kohana 3.2 so there are a few things to note:
 
-- Request, Route and route parameters dependency injection added
-- URL::query() has been removed, Pagination::query() added instead (HMVC support)
+- Fixed problem when used route for pagination
+
+
+## Route example
+
+Bootstrap
+
+    Route::set('pagination', '<controller>(/<action>)/page(/<page>)',
+             array(
+                   'page' => '\d+'
+             )
+    );
+
+
+Config file
+
+    'default' => array(
+              'current_page'      => array('source' => 'query_string', 'key' => 'page'), // source: "query_string" or "route"
+              'total_items'       => 0,
+              'items_per_page'    => 10,
+              'view'              => 'pagination/basic',
+              'auto_hide'         => TRUE,
+              'first_page_in_url' => FALSE,
+    )
--- config/pagination.php
+++ config/pagination.php
@@ -1,15 +1,34 @@
 <?php defined('SYSPATH') or die('No direct script access.');
-
+/**
+ * Added "route_name" key in current_page
+ * Route name to get routing correctly
+ * 
+ * @example
+ * 
+ * bootstrap:
+ * 
+ * Route::set('pagination', '<controller>(/<action>)/page(/<page>)',
+         array(
+               'page' => '\d+'
+         )
+);
+ * 
+ * config pagination:
+ * 
+ * 'current_page'      => array('source' => 'route', 'key' => 'page', 'route_name' => 'pagination'),
+ * 
+ *  @since 3.2
+ */
 return array(
 
-	// Application defaults
-	'default' => array(
-		'current_page'      => array('source' => 'query_string', 'key' => 'page'), // source: "query_string" or "route"
-		'total_items'       => 0,
-		'items_per_page'    => 10,
-		'view'              => 'pagination/basic',
-		'auto_hide'         => TRUE,
-		'first_page_in_url' => FALSE,
-	),
+          // Application defaults
+          'default' => array(
+                    'current_page'      => array('source' => 'route', 'key' => 'page', 'route_name' => 'pagination'), // source: "query_string" or "route"
+                    'total_items'       => 0,
+                    'items_per_page'    => 10,
+                    'view'              => 'pagination/basic',
+                    'auto_hide'         => TRUE,
+                    'first_page_in_url' => FALSE,
+          ),
 
 );
